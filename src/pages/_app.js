925a05a94b67b45987e1ad4632b5eb0d56a7c836
༺𝕷𝖔𝖜𝖐𝖊𝖞 𝕴𝖘 𝕳𝖎𝖒༻⁩ <152390226+Bhadbanks@users.noreply.github.com>
2025-10-21 09:43:58 -0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,31 @@
+// src/pages/_app.js
+import Head from 'next/head';
 import { AuthProvider } from '../utils/AuthContext';
 import '../styles/globals.css';
-import { Toaster } from 'react-hot-toast'; 
+import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Head>
+        <link rel="icon" href="/logo.png" />
+      </Head>
       <Component {...pageProps} />
-      {/* Global Toast Provider and Custom Styles */}
       <Toaster 
-        position="top-center" // Changed to top-center for better visibility
-        reverseOrder={false} 
-        toastOptions={{ 
-            style: { 
-                background: '#333', 
-                color: '#fff',
-                borderRadius: '8px'
-            } 
-        }} 
+        position="top-right"
+        toastOptions={{
+          style: {
+            background: '#333',
+            color: '#fff',
+            border: '1px solid #E91E63',
+          },
+          success: {
+            iconTheme: {
+              primary: '#9C27B0',
+              secondary: '#fff',
+            },
+          },
+        }}
       />
     </AuthProvider>
   );
